0849e3a107cbd926eab5c984414f2892c06317aa
Hitesh Pamnani <hitesh.pamnani@janitri.in>
2022-02-12 16:47:10 +0530

    added initial structure & dependencies

---
+++ app/src/main/java/in/obvious/nasapicx/activity/HomeActivity.java
@@ -0,0 +1,16 @@
+package in.obvious.nasapicx.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import in.obvious.nasapicx.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
